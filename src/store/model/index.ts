af1c36ac1bc1ba71f6e514383b12059dba767f9c
Nods <taxci79@gmail.com>
2020-02-05 00:25:05 +0900

    User base type

--- src/store/model/index.ts
+++ src/store/model/index.ts
@@ -4,19 +4,8 @@ import { ErrorModel } from './error.model';
 import { MealModel } from './meal.model';
 import { NoticeModel } from './notice.model';
 import { TimeTableModel } from './timeTable.model';
-import { UserModel } from './user.model';
+import { UserState } from './user.model';
 import { UtilModel } from './util.model';
-
-export interface AppState {
-  user: UserModel;
-  calendar: CalendarModel;
-  notice: NoticeModel;
-  timeTable: TimeTableModel;
-  meal: MealModel;
-  error: ErrorModel;
-  board: BoardModel;
-  util: UtilModel;
-}
 
 export * from './timeTable.model';
 export * from './user.model';
@@ -27,3 +16,29 @@ export * from './error.model';
 export * from './board.model';
 export * from './util.model';
 export * from './common';
+
+export interface AppState {
+  user: UserState;
+  calendar: CalendarModel;
+  notice: NoticeModel;
+  timeTable: TimeTableModel;
+  meal: MealModel;
+  error: ErrorModel;
+  board: BoardModel;
+  util: UtilModel;
+}
+
+export enum APIStatus {
+  NONE = 'none',
+  PENDING = 'pending',
+  SUCCESS = 'success',
+  FAILURE = 'failure'
+}
+
+export enum APIStatusWithBoolean {
+  NONE = 'none',
+  PENDING = 'pending',
+  SUCCESS_TRUE = 'success-true',
+  SUCCESS_FALSE = 'success-false',
+  FAILURE = 'failure'
+}
